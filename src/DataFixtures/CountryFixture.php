bc1e15f37a4227423af6569f3215014f3b65bb29
Tariq Ayman <tariq.ayman94@gmail.com>
2022-04-27 04:06:17 +0200

    create CountryFixture to fill county table

---
+++ src/DataFixtures/CountryFixture.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Country;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CountryFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $countriesList = [
+            ['code' => '237', 'name' => 'Cameroon', 'regex' => "#^\(237\)\ ?[2368]\d{7,8}$#"],
+            ['code' => '251', 'name' => 'Ethiopia', 'regex' => "#^\(251\)\ ?[1-59]\d{8}$#"],
+            ['code' => '212', 'name' => 'Morocco', 'regex' => "#^\(212\)\ ?[5-9]\d{8}$#"],
+            ['code' => '258', 'name' => 'Mozambique', 'regex' => "#^\(258\)\ ?[28]\d{7,8}$#"],
+            ['code' => '256', 'name' => 'Uganda', 'regex' => "#^\(256\)\ ?\d{9}$#"]
+        ];
+
+        foreach ($countriesList as $country) {
+            $newCountry = new Country();
+
+            $newCountry->setCode($country['code']);
+            $newCountry->setName($country['name']);
+            $newCountry->setRegex($country['regex']);
+
+            $manager->persist($newCountry);
+        }
+
+        $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return ['countryFixture'];
+    }
+}
