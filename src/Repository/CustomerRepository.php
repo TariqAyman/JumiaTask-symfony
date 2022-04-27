2cb47c4201a9d83b3a85eff86355d0613bcb969c
Tariq Ayman <tariq.ayman94@gmail.com>
2022-04-27 04:08:45 +0200

    create customer entity & repository

---
+++ src/Repository/CustomerRepository.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Country;
+use App\Entity\Customer;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Customer>
+ *
+ * @method Customer|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Customer|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Customer[]    findAll()
+ * @method Customer[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CustomerRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Customer::class);
+
+        $this->getEntityManager()
+            ->getConnection()
+            ->getNativeConnection()
+            ->sqliteCreateFunction('REGEXP', function ($pattern, $value) {
+                mb_regex_encoding('UTF-8');
+                return (mb_ereg($pattern, $value)) !== false ? 1 : 0;
+            });
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function add(Customer $entity, bool $flush = true): void
+    {
+        $this->_em->persist($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function remove(Customer $entity, bool $flush = true): void
+    {
+        $this->_em->remove($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    /**
+     * @param $countryCode
+     * @return array Returns an array of Customer objects
+     */
+
+    public function findByCountryCode($countryCode): array
+    {
+        return $this->createQueryBuilder('customer')
+            ->andWhere('customer.phone LIKE :countryCode')
+            ->setParameter('countryCode', '(' . $countryCode . ')%')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * Fetch phone numbers records from database according to the given filters
+     *
+     * @param array $filters
+     * @return Query - filtered phone number
+     */
+    public function queryFetchFilterPhoneNumbers(array $filters)
+    {
+        $countryId = (int) $filters['selectedCountry'];
+
+        $state = $filters['selectedState'] ?? null;
+
+        $queryBuilder = $this->createQueryBuilder('c')
+            ->select([
+                'REGEXP(c.phone,cr.regex) AS state',
+                'cr.name AS countryName',
+                'cr.code AS  countryCode',
+                "SUBSTRING(c.phone,instr(c.phone,' ') +1) AS number"
+            ])
+            ->leftJoin(Country::class, 'cr', 'WITH', "instr(SUBSTRING(c.phone, 1,instr(c.phone,' ') -1),cr.code)>0");
+
+
+        if ($countryId) {
+            $queryBuilder->where('cr.id = :countryId');
+            $queryBuilder->setParameter('countryId', $countryId);
+        }
+
+        if ($state !== 'null') {
+            $queryBuilder->andWhere('REGEXP(c.phone,cr.regex) = :isValid');
+            $queryBuilder->setParameter('isValid', $state, \PDO::PARAM_BOOL);
+        }
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+}
