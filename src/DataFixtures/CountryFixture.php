a7afd6956cd8b575b717fda932b0c69fc8790305
Tariq Ayman <tariq.ayman94@gmail.com>
2022-04-28 02:10:59 +0200

    fix regex code in CountryFixture

--- src/DataFixtures/CountryFixture.php
+++ src/DataFixtures/CountryFixture.php
@@ -12,11 +12,11 @@ class CountryFixture extends Fixture implements FixtureGroupInterface
     public function load(ObjectManager $manager): void
     {
         $countriesList = [
-            ['code' => '237', 'name' => 'Cameroon', 'regex' => "#^\(237\)\ ?[2368]\d{7,8}$#"],
-            ['code' => '251', 'name' => 'Ethiopia', 'regex' => "#^\(251\)\ ?[1-59]\d{8}$#"],
-            ['code' => '212', 'name' => 'Morocco', 'regex' => "#^\(212\)\ ?[5-9]\d{8}$#"],
-            ['code' => '258', 'name' => 'Mozambique', 'regex' => "#^\(258\)\ ?[28]\d{7,8}$#"],
-            ['code' => '256', 'name' => 'Uganda', 'regex' => "#^\(256\)\ ?\d{9}$#"]
+            ['code' => '237', 'name' => 'Cameroon', 'regex' => "^\(237\)\ ?[2368]\d{7,8}$"],
+            ['code' => '251', 'name' => 'Ethiopia', 'regex' => "^\(251\)\ ?[1-59]\d{8}$"],
+            ['code' => '212', 'name' => 'Morocco', 'regex' => "^\(212\)\ ?[5-9]\d{8}$"],
+            ['code' => '258', 'name' => 'Mozambique', 'regex' => "^\(258\)\ ?[28]\d{7,8}$"],
+            ['code' => '256', 'name' => 'Uganda', 'regex' => "^\(256\)\ ?\d{9}$"]
         ];
 
         foreach ($countriesList as $country) {
